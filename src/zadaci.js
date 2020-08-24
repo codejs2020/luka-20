694dd9aba55ffa480da6e49281089bfed7ff41ca
lukautvic <67318904+lukautvic@users.noreply.github.com>
2020-08-24 17:52:56 +0200

    Okej drugi zadatak radi

---
+++ src/zadaci.js
@@ -0,0 +1,18 @@
+// 2. zadatak
+
+const samoglasnici = ['a', 'e', 'i', 'o','u']
+
+function vowelCount (inputText) {
+    let count = 0
+
+    for (let letter of inputText){
+        if (samoglasnici.includes(letter)){
+            count++
+        }
+    }
+    console.log(`This text has ${count} vowels`)
+    return count
+}
+
+vowelCount('luka')
+
