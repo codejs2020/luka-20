6eb6ade8ae0d07ca1b8bcdc97b71deea32a73cb0
lukautvic <67318904+lukautvic@users.noreply.github.com>
2020-08-29 22:23:21 +0200

    populated grid with pics(random positions)

---
+++ src/memory/app.js
@@ -0,0 +1,44 @@
+let cards = []
+
+for (let i = 0; i < 6; i++) {
+    let card = `C:\Images\\${i}.png`
+    cards.push(card)
+    cards.push(card)
+}
+randomCards(cards)
+//console.log(cards)
+//console.log(randomCards(cards))
+
+
+// style='display: none;' used in <img /> tag to hide img
+
+function drawBoard() {
+    let html = '<ol>'
+    for (let i = 0; i < 12; i++) {
+        html += '<li>'
+        html += `<img src = '${cards[i]}' />`
+        html += '</li>'
+    }
+    html += '</ol>'
+    return document.getElementById('container').innerHTML = html
+}
+
+drawBoard()
+
+// Fisher-Yates shuffle algor
+
+function randomCards(arr) {
+
+    let currentIndex = arr.length, temp, randomIndex
+
+    while (0 != currentIndex) {
+        randomIndex = Math.floor(Math.random() * (currentIndex - 1))
+        currentIndex -= 1
+
+
+        temp = arr[currentIndex]
+        arr[currentIndex] = arr[randomIndex]
+        arr[randomIndex] = temp
+    }
+    return arr
+}
