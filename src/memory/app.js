3cab82273c25a8a639b834755a2a41cd13f97360
lukautvic <67318904+lukautvic@users.noreply.github.com>
2020-08-30 00:16:38 +0200

    minor changes

--- src/memory/app.js
+++ src/memory/app.js
@@ -1,31 +1,33 @@
 let cards = []
 let cardNum = 8
-for (let i = 0; i < cardNum; i++) {
-    let card = `http://127.0.0.1:8080//${i}.png`
-    cards.push(card)
-    cards.push(card)
+
+function cardImport (){
+    for (let i = 0; i < cardNum; i++) {
+        let card = `http://127.0.0.1:8080//${i}.png`
+        cards.push(card)
+        cards.push(card)
+    }
+    randomCards(cards)
 }
-randomCards(cards)
-//console.log(cards)
-//console.log(randomCards(cards))
+
 
 
 // style='display: none;' used in <img /> tag to hide img
 
 
-function drawBoard() {
-    let level = 16
+function drawGrid() {
+    let gridSize = 16
     let html = '<div id="list-div">'
-    for (let i = 0; i < level; i++) {
+    for (let i = 0; i < gridSize; i++) {
         html += '<div>'
         html += `<img class='img' src = '${cards[i]}' />`
         html += '</div>'
     }
     html += '</div>'
-    return document.getElementById('container').innerHTML = html
+    document.getElementById('container').innerHTML=html
 }
 
-drawBoard()
+
 
 // Fisher-Yates shuffle algor
 
@@ -44,3 +46,10 @@ function randomCards(arr) {
     }
     return arr
 }
+
+function startGame () {
+    cardImport()
+    drawGrid()
+}
+
+startGame()
