40629edc56b12f13e16a42af848b2fe6aff6d371
lukautvic <67318904+lukautvic@users.noreply.github.com>
2020-08-30 01:07:03 +0200

    odradio animaciju za classList('visible')

--- src/memory/app.js
+++ src/memory/app.js
@@ -1,7 +1,15 @@
 let cards = []
-let cardNum = 8
 
-function cardImport (){
+function clickOnCard(event) {
+    const el = event.target
+    el.classList.add('visible')
+
+    
+}
+
+
+function cardImport() {
+    let cardNum = 8
     for (let i = 0; i < cardNum; i++) {
         let card = `http://127.0.0.1:8080//${i}.png`
         cards.push(card)
@@ -11,23 +19,17 @@ function cardImport (){
 }
 
 
-
-// style='display: none;' used in <img /> tag to hide img
-
-
 function drawGrid() {
     let gridSize = 16
     let html = '<div id="list-div">'
     for (let i = 0; i < gridSize; i++) {
         html += '<div>'
-        html += `<img class='img' src = '${cards[i]}' />`
+        html += `<img id='img' src = '${cards[i]}' />`
         html += '</div>'
     }
     html += '</div>'
-    document.getElementById('container').innerHTML=html
+    document.getElementById('container').innerHTML = html
 }
-
-
 
 // Fisher-Yates shuffle algor
 
@@ -47,9 +49,11 @@ function randomCards(arr) {
     return arr
 }
 
-function startGame () {
+function startGame() {
     cardImport()
     drawGrid()
 }
+
+document.getElementById('container').addEventListener('click',clickOnCard)
 
 startGame()
